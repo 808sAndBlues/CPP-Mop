ddadfcd168230a6afe1c115ac72c86ef4e6e9034
808sAndBlues <ojeetdeol13@gmail.com>
2024-10-13 20:13:11 -0700

    accepting cmd line arguments

--- cpp-mop/src/main.rs
+++ cpp-mop/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::Write;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let cmd_line_args: Vec<String> = extracted_args(&args, args.len());
 }
+
+fn extracted_args(args: &Vec<String>, length: usize) -> Vec<String> {
+   return args[..length].to_vec()
+}
+
+fn debug_vec(args: &Vec<String>) {
+    for i in 0..args.len() {
+        println!("{}", args[i]);
+    }
+}
+
